14a2d1c325cdadb124ca38670f3d6793f271c20b
unknown <brunocampos210@gmail.com>
2022-08-01 13:24:15 -0300

    commit inicial

---
+++ financial-management/src/test/java/br/com/alura/financialmanagement/FinancialManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.financialmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
